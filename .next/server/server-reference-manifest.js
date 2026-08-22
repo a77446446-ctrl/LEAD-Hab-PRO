7a23fff8cd62d6a9d90ac323dc5e9b236a48053b
Admin <admin@example.com>
2026-08-22 23:35:05 +0300

    Switch to @prisma/adapter-pg to satisfy Prisma 7 engine type requirements

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5f4xAtMrNApDPef187RT1cC8jiDyFL/MGFG+ifjTwUI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"ur55e3VMTdN4oGKP12GvIOkbwLzbI2fxQ4nm9cifHo4=\"}"
